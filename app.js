d4b4007dfbd5e432418e0fb57f1d9c05c192d0f9
paflou <pantelis200317@gmail.com>
2024-12-18 19:35:33 +0200

    hotfix + video compression

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/", indexRouter);
 app.use("/login", loginRouter);
 app.use("/users", usersRouter);
--- db/create.sql
+++ db/create.sql
@@ -1,35 +1,60 @@
-DROP DATABASE diplomaDB;
-
-CREATE DATABASE diplomaDB;
+CREATE DATABASE IF NOT EXISTS diplomaDB;
 USE diplomaDB;
 
+DROP TABLE IF EXISTS professor;
+DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS secretary;
+DROP TABLE IF EXISTS user;
+
 CREATE TABLE IF NOT EXISTS user (
-username VARCHAR(255),
-password VARCHAR(255) NOT NULL,
-
-PRIMARY KEY (username)
-);
-
-CREATE TABLE IF NOT EXISTS professor (
+id INT,
 username VARCHAR(255),
 password VARCHAR(255) NOT NULL,
 name VARCHAR(50),
 surname VARCHAR(50),
 email VARCHAR(30),
-topic VARCHAR(255),
 landline INT,
 mobile INT,
+role ENUM('student','professor','secretary') NOT NULL,
+
+PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS professor (
+id INT,
+topic VARCHAR(255),
 department VARCHAR(50),
 university VARCHAR(50),
 
-PRIMARY KEY (username),
-CONSTRAINT prof_username
-FOREIGN KEY (username)
-REFERENCES user(username)
-ON DELETE CASCADE ON UPDATE CASCADE,
+PRIMARY KEY (id),
+CONSTRAINT prof_id
+FOREIGN KEY (id)
+REFERENCES user(id)
+ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-CONSTRAINT prof_pass
-FOREIGN KEY (password)
-REFERENCES user(password)
+CREATE TABLE IF NOT EXISTS student (
+id INT,
+student_number INT,
+street VARCHAR(50),
+street_number INT,
+city VARCHAR(50),
+postcode INT,
+father_name VARCHAR(50),
+
+PRIMARY KEY (id),
+CONSTRAINT student_id
+FOREIGN KEY (id)
+REFERENCES user(id)
+ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS secretary (
+id INT,
+
+PRIMARY KEY (id),
+CONSTRAINT secretary_id
+FOREIGN KEY (id)
+REFERENCES user(id)
 ON DELETE CASCADE ON UPDATE CASCADE
 );
