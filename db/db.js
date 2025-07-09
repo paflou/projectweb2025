68989a433018ae0a288be3b04c5d124bf70a75ba
paflou <pantelis200317@gmail.com>
2025-07-09 12:35:46 +0300

    add automatic DB seeding script

--- db/db.js
+++ db/db.js
@@ -7,7 +7,9 @@ const pool = mariadb.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  multipleStatements: true,
   connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT || '5')
 });
+
 
 module.exports = pool;
---
+++ db/seedDatabase.js
@@ -0,0 +1,50 @@
+require('dotenv').config(); // Load env variables
+const pool = require("./db");
+const mariadb = require('mariadb');
+const fs = require('fs').promises;
+const path = require('path');
+
+const createFilePath = path.join(__dirname, 'create.sql'); // __dirname = script folder
+const insertFilePath = path.join(__dirname, 'insert.sql'); // __dirname = script folder
+
+async function seedDatabase() {
+  const conn = await pool.getConnection();
+  try {
+    // Create db query
+    let sql = await fs.readFile(createFilePath, 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(data);
+    })
+    //Execute the query
+    let rows = await conn.query(sql);
+    
+    // Insert db query
+    sql = await fs.readFile(insertFilePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+    })
+
+    //Execute the query
+    rows = await conn.query(sql);
+    conn.release();
+
+    // Return the first row if found, otherwise null
+    if (rows.length > 0) {
+      return rows[0];
+    } else {
+      return null;
+    }
+  } catch (err) {
+    // Release the connection and propagate the error
+    conn.release();
+    throw err;
+  }
+}
+
+seedDatabase();
