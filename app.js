340febf6e5c707c15cd0f878cc34a4caf6a602e4
Pantelis Flouris <68327978+paflou@users.noreply.github.com>
2025-09-20 15:37:00 +0300

    removed cookie-parser from package.json

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Import required modules
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const fs = require('fs');
 
